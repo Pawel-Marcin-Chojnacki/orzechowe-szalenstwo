25e3c2a1fe1056d94232ecd6e815fbca1df565e6
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2017-09-10 14:28:18 +0200

    First commit from local repo

---
+++ orzechowe-szalenstwo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace orzechowe_szalenstwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
